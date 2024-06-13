d657f7f862e1c3501550237f69c5ed1e8d6b0c32
DavidBrameijer <davidbrameijer@gmail.com>
2024-06-13 14:10:44 -0400

    deliv2

--- Deliverable1/Deliverable1/obj/Debug/net8.0/Deliverable1.AssemblyInfo.cs
+++ Deliverable1/Deliverable1/obj/Debug/net8.0/Deliverable1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Deliverable1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a729e9629c94468cf3c9f15af9f01f29bc472d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9575cf5f6a311dcf86cc8ef210aa8534b48fe5c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Deliverable1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Deliverable1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
